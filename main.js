194489302eb3dab8207cc93fcdf5e6720e89fc11
dieuwbak <dieuwertjeddab@hotmail.nl>
2018-06-20 21:32:03 +0200

    Cleaning up

--- main.js
+++ main.js
@@ -1,350 +1,134 @@
-var map;
-  function initMap() {       
-    var myStyles =[
-                {
-                  "elementType": "geometry",
-                  "stylers": [
-                    {
-                      "color": "#ebe3cd"
-                    }
-                  ]
-                },
-                {
-                  "elementType": "labels.text.fill",
-                  "stylers": [
-                    {
-                      "color": "#523735"
-                    }
-                  ]
-                },
-                {
-                  "elementType": "labels.text.stroke",
-                  "stylers": [
-                    {
-                      "color": "#f5f1e6"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "administrative",
-                  "elementType": "geometry.stroke",
-                  "stylers": [
-                    {
-                      "color": "#c9b2a6"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "administrative.land_parcel",
-                  "stylers": [
-                    {
-                      "visibility": "off"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "administrative.land_parcel",
-                  "elementType": "geometry.stroke",
-                  "stylers": [
-                    {
-                      "color": "#dcd2be"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "administrative.land_parcel",
-                  "elementType": "labels.text.fill",
-                  "stylers": [
-                    {
-                      "color": "#ae9e90"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "administrative.neighborhood",
-                  "stylers": [
-                    {
-                      "visibility": "off"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "landscape.natural",
-                  "elementType": "geometry",
-                  "stylers": [
-                    {
-                      "color": "#dfd2ae"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "poi",
-                  "elementType": "geometry",
-                  "stylers": [
-                    {
-                      "color": "#dfd2ae"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "poi",
-                  "elementType": "labels.text.fill",
-                  "stylers": [
-                    {
-                      "color": "#93817c"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "poi.park",
-                  "elementType": "geometry.fill",
-                  "stylers": [
-                    {
-                      "color": "#a5b076"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "poi.park",
-                  "elementType": "labels.text.fill",
-                  "stylers": [
-                    {
-                      "color": "#447530"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "road",
-                  "elementType": "geometry",
-                  "stylers": [
-                    {
-                      "color": "#f5f1e6"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "road",
-                  "elementType": "labels",
-                  "stylers": [
-                    {
-                      "visibility": "off"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "road.arterial",
-                  "elementType": "geometry",
-                  "stylers": [
-                    {
-                      "color": "#fdfcf8"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "road.highway",
-                  "elementType": "geometry",
-                  "stylers": [
-                    {
-                      "color": "#f8c967"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "road.highway",
-                  "elementType": "geometry.stroke",
-                  "stylers": [
-                    {
-                      "color": "#e9bc62"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "road.highway.controlled_access",
-                  "elementType": "geometry",
-                  "stylers": [
-                    {
-                      "color": "#e98d58"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "road.highway.controlled_access",
-                  "elementType": "geometry.stroke",
-                  "stylers": [
-                    {
-                      "color": "#db8555"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "road.local",
-                  "elementType": "labels.text.fill",
-                  "stylers": [
-                    {
-                      "color": "#806b63"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "transit.line",
-                  "elementType": "geometry",
-                  "stylers": [
-                    {
-                      "color": "#dfd2ae"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "transit.line",
-                  "elementType": "labels.text.fill",
-                  "stylers": [
-                    {
-                      "color": "#8f7d77"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "transit.line",
-                  "elementType": "labels.text.stroke",
-                  "stylers": [
-                    {
-                      "color": "#ebe3cd"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "transit.station",
-                  "elementType": "geometry",
-                  "stylers": [
-                    {
-                      "color": "#dfd2ae"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "water",
-                  "elementType": "geometry.fill",
-                  "stylers": [
-                    {
-                      "color": "#b9d3c2"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "water",
-                  "elementType": "labels.text",
-                  "stylers": [
-                    {
-                      "visibility": "off"
-                    }
-                  ]
-                },
-                {
-                  "featureType": "water",
-                  "elementType": "labels.text.fill",
-                  "stylers": [
-                    {
-                      "color": "#92998d"
-                    }
-                  ]
-                }
-              ];
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
 
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-    map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      clickableIcons: false,
-      styles: myStyles,
-      fullscreenControl: false,
-      center: {lat: 36.102375, lng: -89.978027},
-      mapTypeId: 'terrain'
-    });
+function initAutocomplete() {
+var map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: -33.8688, lng: 151.2195},
+  zoom: 13,
+  mapTypeId: 'roadmap'
+});
 
-    // Create a <script> tag and set the USGS URL as the source.
-    var script = document.createElement('script');
+// Create the search box and link it to the UI element.
+var input = document.getElementById('pac-input');
+var searchBox = new google.maps.places.SearchBox(input);
+map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-    // This example uses a local copy of the GeoJSON stored at
-    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-    document.getElementsByTagName('head')[0].appendChild(script);
+// Bias the SearchBox results towards current map's viewport.
+map.addListener('bounds_changed', function() {
+  searchBox.setBounds(map.getBounds());
+});
 
-                    function getAPIdata() {
+var markers = [];
+// Listen for the event fired when the user selects a prediction and retrieve
+// more details for that place.
+searchBox.addListener('places_changed', function() {
+  var places = searchBox.getPlaces();
 
-                    var url = "http://api.openweathermap.org/data/2.5/weather";
-                    var apiKey ="cb0c741b8289fadab023979b7d8b2048";
-                    var city = document.getElementById("city").value;
+  if (places.length == 0) {
+    return;
+  }
 
-                    // construct request
-                    var request = url + "?" + "appid=" + apiKey + "&" + "q=" + city;
-                    
-                    // get current weather
-                    fetch(request)
-                    
-                    // parse to JSON format
-                    .then(function(response) {
-                      return response.json();
-                    })
-                    
-                    // render weather per day
-                    .then(function(response) {
-                      // render weatherCondition
-                      onAPISucces(response); 
-                    })
-                    
-                    // catch error
-                    .catch(function (error) {
-                      onAPIError(error);
-                    });
-                  }
+  // Clear out the old markers.
+  markers.forEach(function(marker) {
+    marker.setMap(null);
+  });
+  markers = [];
 
+  // For each place, get the icon, name and location.
+  var bounds = new google.maps.LatLngBounds();
+  places.forEach(function(place) {
+    if (!place.geometry) {
+      console.log("Returned place contains no geometry");
+      return;
+    }
+    var icon = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
 
-                  function onAPISucces(response) {
-                    var city = document.getElementById("city").value;
-                    // get type of weather in string format
-                    var type = response.weather[0].description;
+    // Create a marker for each place.
+    markers.push(new google.maps.Marker({
+      map: map,
+      icon: icon,
+      title: place.name,
+      position: place.geometry.location
+    }));
 
-                    // get temperature in Celcius
-                    var degC = Math.floor(response.main.temp - 273.15);
-
-                    // render weather in DOM
-                    var weatherBox = document.getElementById('weather');
-                    weatherBox.innerHTML = degC + "&#176;C <br>" + type;
+    if (place.geometry.viewport) {
+      // Only geocodes have viewport.
+      bounds.union(place.geometry.viewport);
+    } else {
+      bounds.extend(place.geometry.location);
+    }
+  });
+  map.fitBounds(bounds);
+});
+}
 
 
 
-                  }
+function getAPIdata() {
+
+var url = "http://api.openweathermap.org/data/2.5/weather";
+var apiKey ="cb0c741b8289fadab023979b7d8b2048";
+var city = document.getElementById("pac-input").value;
+
+// construct request
+var request = url + "?" + "appid=" + apiKey + "&" + "q=" + city;
+
+// get current weather
+fetch(request)
+
+// parse to JSON format
+.then(function(response) {
+  return response.json();
+})
+
+// render weather per day
+.then(function(response) {
+  // render weatherCondition
+  onAPISucces(response); 
+})
+
+// catch error
+.catch(function (error) {
+  onAPIError(error);
+});
+}
 
 
-                  function onAPIError(error) {
-                    var city = document.getElementById("city").value;
-                    console.error('Fetch request failed', error);
-                    var weatherBox = document.getElementById('weather');
-                    weatherBox.innerHTML = 'No weather data available <br /> Did you enter a valid city?'; 
-                  }
+function onAPISucces(response) {
+var city = document.getElementById("pac-input").value;
+// get type of weather in string format
+var type = response.weather[0].description;
 
-                  document.getElementById("getWeather").onclick = function () {
-                      getAPIdata(city);
-                  };
-   
-    var landingPlaces = [
-        {
-            name: 'Amsterdam'
-            , location: {
-                lat:  52.3702157
-                , lng: 4.8951679
-            }
-        , }
-, ];
+// get temperature in Celcius
+var degC = Math.floor(response.main.temp - 273.15);
 
-    var locationOne = new google.maps.Marker({
-        position: landingPlaces[0].location
-        , map: map
-        , label: 'A'
-        , animation: google.maps.Animation.DROP
-                , name: 'Amsterdam'
+// render weather in DOM
+var weatherBox = document.getElementById('weather');
+weatherBox.innerHTML = degC + "&#176;C <br>" + type;
 
-    , });
+
 
 }
 
-          
+
+function onAPIError(error) {
+var city = document.getElementById("pac-input").value;
+console.error('Fetch request failed', error);
+var weatherBox = document.getElementById('weather');
+weatherBox.innerHTML = 'No weather data available <br /> Did you enter a valid city?'; 
+}
+
+document.getElementById("getWeather").onclick = function () {
+  getAPIdata(city);
+};
